ec8d55dcd920252b76e5805cc2bd3880e0ceee80
dannypho <166521238+dannypho@users.noreply.github.com>
2024-11-09 19:52:11 +0000

    open, close, ls, info

--- mfs.c
+++ mfs.c
@@ -1,6 +1,191 @@
-int main( int argc, char * argv[] )
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+
+#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
+                                // so we need to define what delimits our tokens.
+                                // In this case  white space
+                                // will separate the tokens on our command line
+
+#define MAX_COMMAND_SIZE 255    // The maximum command-line size
+#define MAX_NUM_ARGUMENTS 5
+
+FILE *fp;
+int open = 0;
+
+char BS_OEMName[8];
+int16_t BPB_BytsPerSec;
+int8_t BPB_SecPerClus;
+int16_t BPB_RsvdSecCnt;
+int8_t BPB_NumFATs;
+int16_t BPB_RootEntCnt;
+char BS_VolLab[11];
+int32_t BPB_FATSz32;
+int32_t BPB_ExtFlags;
+int32_t BPB_RootClus;
+int32_t BPB_FSInfo;
+
+int32_t RootDirSectors = 0;
+int32_t FirstDataSector = 0;
+int32_t FirstSectorofCluster = 0;
+
+struct __attribute__((__packed__)) DirectoryEntry {
+  char DIR_Name[11];
+  uint8_t DIR_Attr;
+  uint8_t Unused1[8];
+  uint16_t DIR_FirstClusterHigh;
+  uint8_t Unused2[4];
+  uint16_t DIR_FirstClusterLow;
+  uint32_t DIR_FileSize;
+};
+struct DirectoryEntry dir[16];
+
+void open_fat(char *filename)
 {
-  return 0;
+  fp = fopen(filename, "r+");
+  if ( fp == NULL )
+  {
+    printf("Error: File system image not found.\n");
+  }
+  else if( open == 1 )
+  {
+    printf("Error: File system image already open.\n");
+  }
+  else
+  {
+    fseek(fp, 11, SEEK_SET);
+    fread(&BPB_BytsPerSec, 2, 1, fp);
+
+    fseek(fp, 13, SEEK_SET);
+    fread(&BPB_SecPerClus, 1, 1, fp);
+
+    fseek(fp, 14, SEEK_SET);
+    fread(&BPB_RsvdSecCnt, 2, 1, fp);
+
+    fseek(fp, 16, SEEK_SET);
+    fread(&BPB_NumFATs, 1, 1, fp);
+
+    fseek(fp, 36, SEEK_SET);
+    fread(&BPB_FATSz32, 4, 1, fp);
+
+    fseek(fp, 40, SEEK_SET);
+    fread(&BPB_ExtFlags, 2, 1, fp);
+
+    fseek(fp, 44, SEEK_SET);
+    fread(&BPB_RootClus, 4, 1, fp);
+
+    fseek(fp, 48, SEEK_SET);
+    fread(&BPB_FSInfo, 2, 1, fp);
+
+    // Calculate root directory and populate DirectoryEntry struct
+    int64_t offset = (BPB_BytsPerSec * BPB_RsvdSecCnt) + (BPB_BytsPerSec * BPB_FATSz32 * BPB_NumFATs);
+    fseek(fp, offset, SEEK_SET);
+    fread(&dir[0], sizeof(struct DirectoryEntry) * 16, 1, fp);
+
+    open = 1;
+  }
 }
 
+void info()
+{
+  if ( open == 0 )
+  {
+    printf("Error: File system image must be opened first\n");
+    return;
+  }
+  printf("BPB_BytesPerSec \t%d\t0x%x\n", BPB_BytsPerSec, BPB_BytsPerSec);
+  printf("BPB_SecPerClus \t\t%d\t0x%x\n", BPB_SecPerClus, BPB_SecPerClus);
+  printf("BPB_RsvdSecCnt \t\t%d\t0x%x\n", BPB_RsvdSecCnt, BPB_RsvdSecCnt);
+  printf("BPB_NumFATs \t\t%d\t0x%x\n", BPB_NumFATs, BPB_NumFATs);
+  printf("BPB_FATSz32 \t\t%d\t0x%x\n", BPB_FATSz32, BPB_FATSz32);
+  printf("BPB_ExtFlags \t\t%d\t0x%x\n", BPB_ExtFlags, BPB_ExtFlags);
+  printf("BPB_RootClus \t\t%d\t0x%x\n", BPB_RootClus, BPB_RootClus);
+  printf("BPB_FSInfo \t\t%d\t0x%x\n", BPB_FSInfo, BPB_FSInfo);
+}
 
+void close_fat()
+{
+  if ( open == 1 )
+  {
+    fclose(fp);
+    open = 0;
+  }
+  else
+  {
+    printf("Error: File system not open.\n");
+  }
+}
+
+void ls()
+{
+  if ( open == 0 )
+  {
+    printf("Error: File system image must be opened first\n");
+    return;
+  }
+  for( int i = 0; i < 16; i ++)
+  { 
+    if ( (dir[i].DIR_Attr == 1 || dir[i].DIR_Attr == 16 || dir[i].DIR_Attr == 32) && (dir[i].DIR_Name[0] != 0xe5) )
+    {
+      char name[12];
+      strncpy(name, dir[i].DIR_Name, 12);
+      printf("%s\n", name);
+    }
+  }
+}
+
+int main()
+{
+  char * command_string = (char*) malloc( MAX_COMMAND_SIZE );
+  while( 1 )
+  {
+    printf ("mfs> ");
+
+    while( !fgets (command_string, MAX_COMMAND_SIZE, stdin) );
+    char *token[MAX_NUM_ARGUMENTS];
+    int token_count = 0;                                 
+                                                           
+
+    char *argument_pointer;                                         
+                                                           
+    char *working_string  = strdup( command_string );                
+
+    char *head_ptr = working_string;
+    
+    while ( ( (argument_pointer = strsep(&working_string, WHITESPACE ) ) != NULL) &&
+              (token_count<MAX_NUM_ARGUMENTS))
+    {
+      token[token_count] = strndup( argument_pointer, MAX_COMMAND_SIZE );
+      if( strlen( token[token_count] ) == 0 )
+      {
+        token[token_count] = NULL;
+      }
+        token_count++;
+    }
+
+    if( strcmp(token[0], "exit") == 0 || strcmp(token[0], "quit") == 0 )
+    {
+      exit(0);
+    }
+    if( strcmp(token[0], "open") == 0 )
+    {
+      open_fat(token[1]);
+    }
+    if( strcmp(token[0], "close") == 0 )
+    {
+      close_fat();
+    }
+    if( strcmp(token[0], "info") == 0 )
+    {
+      info();
+    }
+    if( strcmp(token[0], "ls") == 0 )
+    {
+      ls();
+    }
+
+    free( head_ptr );
+  }
+  return 0;
+}
